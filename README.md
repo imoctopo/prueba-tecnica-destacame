b184f5f4ebc78fba186e38d77343180115306959
Camilo Fernández <octopo@octopo.cl>
2021-01-14 23:36:12 -0300

    fix

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# prueba-tecnica-destacame
+# Prueba Técnica Destácame
+####Instalar con Docker Compose (se asume que está instalado Docker y Docker Compose). Ejectuar los siguientes comandos.
+
+Paso 1: 
+```
+$ docker-compose build
+```
+Paso 2:
+```
+$ docker-compose up -d
+```
+**Url Frontend:**
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = 'g6u!q(jlqo&y54_*3bue(+lmu2z1wbphv(vkhnefgbub_m8ut8'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:8080",
-    'http://138.68.25.182',
-    'http://138.68.25.182:8080',
-    'http://138.68.25.182:8000'
+    'localhost',
+    'localhost:8000',
+    'localhost:8080',
+    '138.68.25.182',
+    '138.68.25.182:8080',
+    '138.68.25.182:8000'
 ]
 
 # Application definition
@@ -140,8 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django REST Framework
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:8080",
+    'http://localhost',
+    'http://localhost:8080',
     'http://138.68.25.182',
     'http://138.68.25.182:8080',
     'http://138.68.25.182:8000'
